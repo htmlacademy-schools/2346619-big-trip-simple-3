8072261b3c5682a108eb956609882396c9df37f0
Ян Коваленко <kovalenko.yai@edu.sbpstu.ru>
2023-06-06 19:44:43 +0300

    module2-task1

---
+++ src/mock/picture.js
@@ -0,0 +1,9 @@
+import { getRandomPic, getRandomArrayElement } from '../utils';
+import { descriptionPhrases } from './const';
+
+const createPictures = () => ({
+  src: getRandomPic(),
+  description: getRandomArrayElement(descriptionPhrases)
+});
+
+export {createPictures};
