dead7e4b08c918f124a7760296278fbceb295c9e
Ян Коваленко <kovalenko.yai@edu.sbpstu.ru>
2023-06-06 21:22:51 +0300

    module3-task1

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -1,29 +1,73 @@
 import SortView from '../view/sort-view';
-import PointsListView from '../view/points-list-view';
+import PointListView from '../view/point-list-view';
 import PointView from '../view/point-view';
 import PointEditorView from '../view/point-editor-view';
-import NewItemFormView from '../view/form-creator-view';
+import CreateFormView from '../view/form-creator-view';
+import NoPointsView from '../view/no-points-view';
 import { render } from '../render';
+import { isEscapeKey } from '../utils';
 
 export default class BoardPresenter {
-  eventListComponent = new PointsListView();
+  #boardContainer = null;
+  #tripPointsModel = null;
+  #eventListComponent = null;
+  #tripPoints = null;
 
   constructor({boardContainer, tripPointsModel}) {
-    this.boardContainer = boardContainer;
-    this.tripPointsModel = tripPointsModel;
+    this.#boardContainer = boardContainer;
+    this.#tripPointsModel = tripPointsModel;
   }
 
   init() {
-    this.tripPoints = [...this.tripPointsModel.getTripPoints()];
-
-    render(new SortView(), this.boardContainer);
-    render(this.eventListComponent, this.boardContainer);
-
-    render(new NewItemFormView(), this.eventListComponent.getElement());
-    render(new PointEditorView(), this.eventListComponent.getElement());
-
-    for (let i = 0; i < this.tripPoints.length; i++) {
-      render(new PointView({tripPoint: this.tripPoints[i]}), this.eventListComponent.getElement());
+    this.#tripPoints = [...this.#tripPointsModel.tripPoints];
+    if (this.#tripPoints.length === 0) {
+      render(new NoPointsView(), this.#boardContainer);
+    } else {
+      this.#eventListComponent = new PointListView();
+      render(new SortView(), this.#boardContainer);
+      render(this.#eventListComponent, this.#boardContainer);
+      render(new CreateFormView(this.#tripPoints[0]), this.#eventListComponent.element);
+      for (let i = 1; i < this.#tripPoints.length - 1; i++) {
+        this.#renderTripPoint(this.#tripPoints[i]);
+      }
     }
+  }
+
+  #renderTripPoint(tripPoint) {
+    const tripPointComponent = new PointView({tripPoint});
+    const editFormComponent = new PointEditorView(tripPoint);
+
+    const replacePointToForm = () => {
+      this.#eventListComponent.element.replaceChild(editFormComponent.element, tripPointComponent.element);
+    };
+
+    const replaceFormToPoint = () => {
+      this.#eventListComponent.element.replaceChild(tripPointComponent.element, editFormComponent.element);
+    };
+
+    tripPointComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replacePointToForm();
+      document.body.addEventListener('keydown', closeEditFormOnEcsapeKey);
+    });
+
+    editFormComponent.element.querySelector('.event__save-btn').addEventListener('click', (evt) => {
+      evt.preventDefault();
+      replaceFormToPoint();
+      document.body.removeEventListener('keydown', closeEditFormOnEcsapeKey);
+    });
+
+    editFormComponent.element.querySelector('.event__rollup-btn').addEventListener('click', () => {
+      replaceFormToPoint();
+      document.body.removeEventListener('keydown', closeEditFormOnEcsapeKey);
+    });
+
+    function closeEditFormOnEcsapeKey(evt) {
+      if (isEscapeKey(evt)) {
+        evt.preventDefault();
+        replaceFormToPoint();
+        document.body.removeEventListener('keydown', closeEditFormOnEcsapeKey);
+      }
+    }
+    render(tripPointComponent, this.#eventListComponent.element);
   }
 }
--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -28,16 +28,18 @@ function createSortTemplate() {
 }
 
 export default class SortView {
-  getTemplate() {
-    return createSortTemplate();
+  #element = null;
+
+  get template() {
+    return createSortTemplate;
   }
 
-  getElement() {
-    if (!this.element) {
-      this.element = createElement(this.getTemplate());
+  get element() {
+    if (!this.#element) {
+      this.#element = createElement(this.template);
     }
 
-    return this.element;
+    return this.#element;
   }
 
   removeElement() {
