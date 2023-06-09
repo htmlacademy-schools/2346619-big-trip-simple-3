5e05184077316faca5117ff4667f75947c637f92
Ян Коваленко <kovalenko.yai@edu.sbpstu.ru>
2023-06-09 15:03:39 +0300

    module8-task1

--- src/presenter/board-presenter.js
+++ src/presenter/board-presenter.js
@@ -3,10 +3,11 @@ import PointListView from '../view/point-list-view';
 import NoPointsView from '../view/no-points-view';
 import {render, RenderPosition, remove} from '../framework/render';
 import PointPresenter from '../presenter/point-presenter';
-import {FilterType, SortType, UpdateType, UserAction} from '../mock/const';
-import {sorts} from '../mock/sort';
+import {FilterType, SortType, UpdateType, UserAction} from '../const';
+import {sorts} from '../sort';
 import {filter} from '../utils';
 import NewPointPresenter from './new-point-presenter';
+import LoadingView from '../view/loading-view';
 
 export default class BoardPresenter {
   #noPoints = null;
@@ -22,8 +23,8 @@ export default class BoardPresenter {
   #currentSortType = SortType.DAY;
   #filterType = FilterType.EVERYTHING;
 
-  #offers = [];
-  #destinations = [];
+  #loadingComponent = new LoadingView();
+  #isLoading = true;
 
   #modelFilter = null;
   #newPointPresenter = null;
@@ -51,16 +52,22 @@ export default class BoardPresenter {
     return (sorts[this.#currentSortType]) ? filteredPoints.sort(sorts[this.#currentSortType]) : filteredPoints;
   }
 
+  get destinations() {
+    return this.#modelDestinations.destinations;
+  }
+
+  get offers() {
+    return this.#modelOffers.offers;
+  }
+
   init() {
-    this.#offers = [...this.#modelOffers.offers];
-    this.#destinations = [...this.#modelDestinations.destinations];
     this.#renderBoard();
   }
 
   createPoint() {
     this.#currentSortType = SortType.DAY;
     this.#modelFilter.setFilter(UpdateType.MAJOR, FilterType.EVERYTHING);
-    this.#newPointPresenter.init(this.#destinations, this.#offers);
+    this.#newPointPresenter.init(this.destinations, this.offers);
   }
 
   #renderNoPoint() {
@@ -90,16 +97,20 @@ export default class BoardPresenter {
   #renderPoint(point) {
     const pointPresenter = new PointPresenter({
       pointList: this.#pointsList.element,
-      offers: this.#offers,
-      destinations: this.#destinations,
+      offers: this.offers,
+      destinations: this.destinations,
       onDataChange: this.#handleViewAction,
       onModeChange: this.#renderHandleModeChange,
     });
-    pointPresenter.init(point, this.#destinations, this.#offers);
+    pointPresenter.init(point, this.destinations, this.offers);
     this.#pointPresenter.set(point.id, pointPresenter);
   }
 
   #renderBoard() {
+    if (this.#isLoading) {
+      this.#renderLoading();
+      return;
+    }
     const points = this.points;
     if (points.length === 0) {
       this.#renderNoPoint();
@@ -109,6 +120,10 @@ export default class BoardPresenter {
 
     render(this.#pointsList, this.#boardContainer);
     this.#renderPointsList(points);
+  }
+
+  #renderLoading() {
+    render(this.#loadingComponent, this.#boardContainer, RenderPosition.AFTERBEGIN);
   }
 
   #handleSortTypeChange = (sortType) => {
@@ -137,7 +152,7 @@ export default class BoardPresenter {
   #handleModelEvent = (updateType, data) => {
     switch (updateType) {
       case UpdateType.PATCH:
-        this.#pointPresenter.get(data.id).init(data, this.#modelDestinations.destinations, this.#modelOffers.offers);
+        this.#pointPresenter.get(data.id).init(data, this.destinations, this.offers);
         break;
       case UpdateType.MINOR:
         this.#clearBoard();
@@ -145,6 +160,11 @@ export default class BoardPresenter {
         break;
       case UpdateType.MAJOR:
         this.#clearBoard({resetSortType: true});
+        this.#renderBoard();
+        break;
+      case UpdateType.INIT:
+        this.#isLoading = false;
+        remove(this.#loadingComponent);
         this.#renderBoard();
         break;
     }
@@ -156,6 +176,7 @@ export default class BoardPresenter {
     this.#pointPresenter.clear();
 
     remove(this.#sort);
+    remove(this.#loadingComponent);
 
     if (this.#noPoints) {
       remove(this.#noPoints);
