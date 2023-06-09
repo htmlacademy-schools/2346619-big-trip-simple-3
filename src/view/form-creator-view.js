389108dab12c1268212e27d495a4c922c5d18d02
Ян Коваленко <kovalenko.yai@edu.sbpstu.ru>
2023-06-09 20:42:49 +0300

    module8-task3

--- src/view/form-creator-view.js
+++ src/view/form-creator-view.js
@@ -93,7 +93,7 @@ function createFormTemplate(tripPoint) {
     </header>
     <section class="event__details">
       <section class="event__section  event__section--offers">
-        <h3 class="event__section-title  event__section-title--offers" ${visibility}">Offers</h3>
+        <h3 class="event__section-title  event__section-title--offers ${visibility}">Offers</h3>
         <div class="event__available-offers">
 
         </div>
